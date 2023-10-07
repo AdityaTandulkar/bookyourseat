efd6207773e7a10b08484846a4f3fa658cdf6414
aditya <adityatandulkar20@gmail.com>
2023-10-07 21:45:25 +0530

    Initial commi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './component/header/Header';
+import Seatbooking from './component/seatbooking/Seatbooking';
+import Status from './component/status/Status';
+import { createContext, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const context = createContext("");
+
+function error(msg){
+  toast.error("Please select " + msg, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+}
+
+function success(seats){
+  toast.success("Congratulation tickets has been booked successfully " + seats, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+  });
+}
+
 
 function App() {
+  const [ticketType, setTicketType] = useState("");
+  const [ticketCount, setTicketCount] = useState(0);
+  const [selectedSeats, setSelectedSeats] = useState([]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <context.Provider value={{ticketType, setTicketType, ticketCount, setTicketCount, error, success, selectedSeats, setSelectedSeats}}>
+      <div className="App">
+        <header className="App-header">
+          <Header />
+        </header>
+
+        <main>
+          <Seatbooking />
+          <Status />
+        </main>
+
+        <ToastContainer progressClassName="toastProgress" bodyClassName="toastBody"/>
+      </div>
+    </context.Provider>
   );
 }
 
